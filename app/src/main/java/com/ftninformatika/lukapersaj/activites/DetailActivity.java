ed7960e6ba266a77a924ed1b2e884cdcbdbad960
Lukap992 <Luka.Persj992@gmail.com>
2019-03-19 20:49:02 +0100

    commit 5

--- app/src/main/java/com/ftninformatika/lukapersaj/activites/DetailActivity.java
+++ app/src/main/java/com/ftninformatika/lukapersaj/activites/DetailActivity.java
@@ -1,15 +1,204 @@
 package com.ftninformatika.lukapersaj.activites;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ftninformatika.lukapersaj.R;
+import com.ftninformatika.lukapersaj.db.DataBaseHelper;
+import com.ftninformatika.lukapersaj.db.model.Grupa;
+import com.ftninformatika.lukapersaj.db.model.TodoZadatak;
+import com.ftninformatika.lukapersaj.model.NavigationItem;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.ForeignCollection;
 
-public class DetailActivity extends Activity {
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private DataBaseHelper dataBaseHelper;
+
+    Grupa grupa;
+
+    ForeignCollection<TodoZadatak> zadatak;
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerListView;
+    private ActionBarDrawerToggle drawerToggle;
+    private CharSequence drawerTitle;
+    private RelativeLayout drawerPane;
+    private ArrayList<NavigationItem> drawerItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        try {
+            prikaziDetaljeGrupe();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void prikaziDetaljeGrupe() throws SQLException{
+        Intent intent = getIntent();
+        int id = intent.getExtras().getInt("position");
+
+        grupa  = getDatabaseHelper().getGrupaDao().queryForId(id);
+
+        TextView nazivGrupe  = findViewById(R.id.detail_naziv);
+        nazivGrupe.setText("Naziv Grupe" + grupa.getNaziv());
+
+        TextView datumGrupe = findViewById(R.id.detail_datum);
+        datumGrupe.setText("Datum kreiranja" + grupa.getDatumKreiranja());
+
+        TextView vremeGrupe = findViewById(R.id.detail_vremeK);
+        vremeGrupe.setText("Vreme kreiranja" + grupa.getVremeKreiranja());
+
+        TextView oznaka = findViewById(R.id.detail_oznaka);
+        oznaka.setText("Oznaka: " + grupa.getOznaka());
+
+        ListView list_zadaci = findViewById(R.id.lista_zadataka);
+        ForeignCollection<TodoZadatak> todoZadatakForeignCollection = getDatabaseHelper().getGrupaDao().queryForId(id).getListaZadatka();
+        List<TodoZadatak> listaZadataka = new ArrayList<>(todoZadatakForeignCollection);
+        ArrayAdapter<TodoZadatak> arrayAdapter = new ArrayAdapter<>(DetailActivity.this, android.R.layout.simple_list_item_1, listaZadataka);
+        list_zadaci.setAdapter(arrayAdapter);
+    }
+
+    private void delete(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.obrisi_grupu);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
+        Button btnYes = dialog.findViewById(R.id.obrisi_btnYes);
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent intent = getIntent();
+                    int id = intent.getExtras().getInt("position");
+
+                    grupa = getDatabaseHelper().getGrupaDao().queryForId(id);
+                    getDatabaseHelper().getGrupaDao().delete(grupa);
+
+                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(DetailActivity.this);
+                    boolean showMessage = sharedPreferences.getBoolean("toast_settings", true);
+                    if (showMessage) {
+                        Toast.makeText(DetailActivity.this, "Uspesno IZBRISANO", Toast.LENGTH_LONG).show();
+                    }
+                    Intent intent1 = new Intent(DetailActivity.this, MainActivity.class);
+                    startActivity(intent1);
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button btnNo = dialog.findViewById(R.id.obrisi_btnNo);
+        btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+    private void update(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.izmeni_grupu);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
+        final EditText nazivGrupe = dialog.findViewById(R.id.dialog_izmeni_naziv);
+        final EditText datumGrupe = dialog.findViewById(R.id.dialog_izmeni_datum);
+        final EditText vremeKreiranja = dialog.findViewById(R.id.dialog_izmeni_vreme);
+        final EditText oznakaGrupe = dialog.findViewById(R.id.dialog_izmeni_oznaku);
+
+        Button btnYes = dialog.findViewById(R.id.dialog_izmeni_btnConfirm);
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (datumGrupe.getText().toString().isEmpty() || !isValidDate(datumGrupe.getText().toString())) {
+                    Toast.makeText(DetailActivity.this, "Date can't be EMPTY - Date Format: dd-MM-yyyy", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                grupa.setNaziv(nazivGrupe.getText().toString());
+                grupa.setDatumKreiranja(datumGrupe.getText().toString());
+                grupa.setVremeKreiranja(Integer.parseInt(vremeKreiranja.getText().toString()));
+                grupa.setOznaka(oznakaGrupe.getText().toString());
+
+                try {
+                    getDatabaseHelper().getGrupaDao().update(grupa);
+
+                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(DetailActivity.this);
+                    boolean showMassage = sharedPreferences.getBoolean("toast_settings", true);
+                    if (showMassage) {
+                        Toast.makeText(DetailActivity.this, "Izmena uspesno zavrsena", Toast.LENGTH_LONG).show();
+                    }
+                    dialog.dismiss();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+    private void navigationDrawer(){
+        Toolbar toolbar = findViewById(R.id.detail_toolbar);
+        setSupportActionBar(toolbar);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_format_list);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.show();
+        }
+        drawerItems.add(new NavigationItem("Prikaz glumaca", "Prikazuje sve grupe", R.drawable.ic_group_black_24dp));
+        drawerItems.add(new NavigationItem("Podesavanja", "Podesavanja aplikacije", R.drawable.ic_settings_black_24dp));
+
+
+    }
+    public static boolean isValidDate(String inDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(inDate.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
+    }
+
+    public DataBaseHelper getDatabaseHelper() {
+        if (dataBaseHelper == null) {
+            dataBaseHelper = OpenHelperManager.getHelper(this, DataBaseHelper.class);
+        }
+        return dataBaseHelper;
     }
 }
